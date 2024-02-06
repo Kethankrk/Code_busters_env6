8f2081fd93b8a9b9bc67c7dd207de954df27acd8
Abhinav shyju <abhinavshyjupc@gmail.com>
2024-02-07 00:01:02 +0530

    Sign up and sign in update

--- healthhub/src/App.jsx
+++ healthhub/src/App.jsx
@@ -3,13 +3,16 @@ import './App.css'
 import HomePage from './pages/homePage/HomePage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Services from './pages/servicesPage/Services'
+import LoginPage from './pages/loginpage/loginPage'
+import Signup from './pages/SignupPage/Signup'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage />} />
+          {/* <Route path='/' element={<HomePage />} /> */}
+          <Route path='/' element={<Signup />} />
           <Route path='/service' element={<Services />} />
         </Routes>
       </BrowserRouter>
